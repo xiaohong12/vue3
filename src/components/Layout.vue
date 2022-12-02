2b9591ae6058f8826657f467c56e7bf248497294
“henghong.jiang” <2595997982@qq.com>
2022-12-02 14:25:13 +0800

    1

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,13 +1,14 @@
 <template>
   <el-menu
     :default-active="activeIndex"
-    class="el-menu-demo"
+    class="el-menu-main"
     mode="horizontal"
     collapse-transition
     background-color='#292b4d'
     text-color='hsla(0,0%,100%,.65)'
     active-text-color='#fff'
   >
+  <div class='el-menu-item mtr50' @click="clickRouter({path:'/'})"><span>Vue3 Project</span></div>
   <template v-for="(item ,key) in routes[0]?.children"
     v-bind:key="key">
     <SubMenuItem :list="item" v-if="item?.children?.length>0" ></SubMenuItem>
@@ -29,8 +30,20 @@ import useMenueRouterStore from '@stores/menueRouter'
 //只能解析state 不能解析方法  方法需要单独调用
 const store=useMenueRouterStore();
 const {activeIndex}=storeToRefs(useMenueRouterStore()) 
-console.log(routes[0])
 const clickRouter=(item)=>{
   store.navationToPage(item)
 }
 </script>
+<style lang="scss" scoped>
+  .el-menu-item.mtr50{
+    margin-right: 50px;
+    span{
+        color: #36cad7 !important;
+        font-size: 20px;
+    }
+  }
+  
+  .el-menu-item.mtr50:hover{
+    background-color: #292b4d !important;
+  }
+</style>
